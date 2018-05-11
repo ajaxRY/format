46aabe9e594223741758128adcd25fa97c2529c1
ry <1234568312@qq.com>
2018-05-11 11:11:56 +0800

    format

---
+++ file/format.js
@@ -0,0 +1,10 @@
+function format(num) {
+    var reg = /^\d{17}(\d|X|x)/;
+    if (reg.test(num)) {
+        var nstr = num.replace(num.substr(-4, 4), "****");
+        var word = nstr.substr(-8, 4);
+        word = '<strong>' + word + '</strong>';
+        var bStr = nstr.replace(nstr.substr(-8, 4), word);
+        return bStr;
+    }
+};
